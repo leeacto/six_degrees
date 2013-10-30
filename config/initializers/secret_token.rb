7a4fc748ec1a93fe055c833d6209bb38e940b904
Nicholas <nicholas.lee.3@gmail.com>
2013-10-30 10:55:59 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SixDegrees::Application.config.secret_key_base = 'adb74c259fcca909ba502175e1698d81e4e3a242d244d7b772c2d1c20d5d564f0f2043f12f5d57f333fbac9b6261e68b1af109e304c754979996913e9e9ab2a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SixDegrees::Application.config.session_store :cookie_store, key: '_six_degrees_session'
